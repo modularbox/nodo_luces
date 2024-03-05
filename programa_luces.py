560e285b5c8e08e8dfe4905b55e140ec61877aef
modularbox  s.l <modularboxs.l@MacBook-Air-de-modularbox.local>
2024-03-05 11:41:56 +0100

    Version 8

--- programa_luces.py
+++ programa_luces.py
@@ -80,10 +80,10 @@ def get_light_state_from_api(data, lugar):
     global guardar_configuracion_luces
     global luces_encendidas
 
-    print(data.get('encender'))
-    print(data)
     if data.get('encender') == None:
-        return Luces(ProgramaHardcode(lugar).get_luces_lugar())
+        if not luces_encendidas:
+            luces_encendidas = True 
+            return Luces(ProgramaHardcode(lugar).get_luces_lugar())
     # Verificar el horario para encender las luces o apagarlas
     if verificar_horarios(data.get('horarios')):
         if not luces_encendidas:

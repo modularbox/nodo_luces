fc587a36bed6b107023080bebb8a3be3a51c3dc7
modularbox  s.l <modularboxs.l@MacBook-Air-de-modularbox.local>
2024-03-06 16:33:21 +0100

    Version 49

--- luces_sockets.py
+++ luces_sockets.py
@@ -39,6 +39,7 @@ class TimedEventThread(threading.Thread):
 
     def run(self):
         while not self.stopped.wait(self.interval):
+            print("Entro ")
             if self.programa_execute == Programas.PROGRAMA:
                 self.programa(self.request_programa)
             elif self.programa_execute == Programas.PROGRAMA_POR_TIEMPO:

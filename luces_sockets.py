ac867fd52d5c00df1df77cab6538e3a667ed0962
Julio <juliofresno@hotmail.com>
2024-03-19 22:50:06 +0100

    Version 2.0.2-1

--- luces_sockets.py
+++ luces_sockets.py
@@ -9,7 +9,7 @@ from programa_hardcode import ProgramaHardcode
 from custom_logger import CustomLogger
 
 # Version Programa
-VERSION = '2.0.1-1'
+VERSION = '2.0.2-1'
 
 # Crear una instancia del logger
 logger = CustomLogger()

6af7111ad7284d41239e7346ad0d0da18ac1cd19
Julio <juliofresno@hotmail.com>
2024-03-20 01:54:37 +0100

    Version 2.0.2-10

--- p2.py
+++ p2.py
@@ -1,3 +1,12 @@
 from PyDMX import *
 
 dmx = PyDMX('COM3') # for Linux use '/dev/ttyUSB0' or something
+
+while True:
+    dmx.set_data(1,255)
+    dmx.set_data(2,255)
+    dmx.set_data(3,255)
+    dmx.set_data(4,255)
+    dmx.send()
+
+    time.sleep(10)

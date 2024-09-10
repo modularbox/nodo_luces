adae153cb6d7f709d4016d9c3073589cfb0c92c2
Miguel <juliofresno@modularbox.com>
2024-09-10 12:44:15 +0200

    Version cambiar custom 5

--- new_code.py
+++ new_code.py
@@ -1,34 +1,29 @@
-
-# Importar el controlador OpenDMX o uDMX de PyDMXControl,
-# este será el medio por el cual se enviarán los datos.
+import time
 from PyDMXControl.controllers import OpenDMXController
-# from PyDMXControl.controllers import uDMXController
-
-# Importar el perfil de fixture que utilizaremos,
-# en este ejemplo, el simple Dimmer.
-from PyDMXControl.profiles.Generic import Dimmer
 from PyDMXControl.profiles.Generic import Custom
 
 # What DMX channels we want to listen to
 dmxChannels = [1, 2, 11, 12, 21, 24, 27, 30, 33, 36, 39, 42, 51, 54, 57, 60, 63, 66, 69, 72, 91, 92, 101, 102]
-# Crear una instancia del controlador OpenDMX,
-# esta instancia mantendrá toda la información de los fixtures y la enviará.
-# Esto comenzará a enviar datos inmediatamente.
+
+# Crear una instancia del controlador OpenDMX
 dmx = OpenDMXController()
-# dmx = uDMXController()
 
 # Añadir un nuevo fixture Dimmer a nuestro controlador
-# y guardarlo en una variable para que podamos acceder a él.
-# Le damos un nombre para que sea más fácil identificarlo en las opciones de control de depuración.
 fixture = dmx.add_fixture(Custom, name="Mi_Primer_Dimmer", start_channel=1, channels=150)
 
-# Ahora, enciende los canales especificados en el array
-# Se asume que quieres encender todos los canales al valor máximo (255)
-dmxChannels = [1, 2, 11, 12, 21, 24, 27, 30, 33, 36, 39, 42, 51, 54, 57, 60, 63, 66, 69, 72, 91, 92, 101, 102]
+# Función para encender los canales
+def turn_on_channels(fixture, channels):
+    fixture.set_channels(*[255 if channel in channels else None for channel in range(1, 513)])
 
-# Establece el valor de cada canal a 255
-fixture.set_channels(*[255 if channel in dmxChannels else None for channel in range(1, 513)])
+# Función para apagar los canales
+def turn_off_channels(fixture, channels):
+    fixture.set_channels(*[0 if channel in channels else None for channel in range(1, 513)])
 
-# O si el número de canales es menor a 512, solo usa los canales necesarios
-# Ajuste de ejemplo en base al número de canales necesarios
-# fixture.set_channels(*[255 if i in dmxChannels else None for i in range(1, max(dmxChannels) + 1)])
+# Encender los canales
+turn_on_channels(fixture, dmxChannels)
+
+# Esperar un tiempo antes de apagar los canales (por ejemplo, 10 segundos)
+time.sleep(10)
+
+# Apagar los canales
+turn_off_channels(fixture, dmxChannels)

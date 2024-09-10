67f00e771e79fd078ff96667db69ffd59d5d1df4
Miguel <juliofresno@modularbox.com>
2024-09-10 21:25:10 +0200

    Version cambiar cversion y quitar datos_guardados y agregar en el ignore

--- luces_sockets.py
+++ luces_sockets.py
@@ -9,7 +9,7 @@ from programa_hardcode import ProgramaHardcode
 from custom_logger import CustomLogger
 
 # Version Programa
-VERSION = '2.0.4-10'
+VERSION = '3.0.0-1'
 
 # Crear una instancia del logger
 logger = CustomLogger()

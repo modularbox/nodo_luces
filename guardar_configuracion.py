63af46918faa2f640cfcc75864065de0a9364690
Miguel <juliofresno@modularbox.com>
2024-11-26 19:48:12 +0100

    Configuracion cruzbendita white

--- guardar_configuracion.py
+++ guardar_configuracion.py
@@ -4,28 +4,24 @@ from custom_logger import CustomLogger
 # Crear una instancia del logger
 logger = CustomLogger()
 inicioProgram = False
+# Colores
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+yellow = [255, 255, 0]
+orange = [255, 170, 0]
+purple = [170, 0, 255]
+apagar = [0, 0, 0]
+white = [255, 255, 255]
+
+
+
 # Aqui viene la configuracion de luces de saucedilla
 # De la iglesia y del ayuntamiento Esta es en orange
 class GuardarConfiguracion:
     def __init__(self, lugar):
         logger.log_info("La configuracion esta en color orange")
-        programIglesia = [1,2,[3,170],11,12,[13,170],
-                          21,22,[23,170],31,32,[33,170],
-                          41,42,[43,170],51,52,[53,170],61,62,
-                          [63,170],71,72,[73,170],81,82,[83,170]
-                          ,91,92,[93,170],101,102,[103,170],111,112,
-                          [113,170],121,122,[123,170],131,132,[133,170],
-                          141,142,[143,170],151,152,[153,170],161,162,
-                          [163,170]]
-        programAyuntamiento = [171,172,[173,170],181,182,[183,170],321,322,
-                               [323,170],331,332,[333,170],341,342,[343,170],
-                               201,[202,170],204,[205,170],207,[208,170],210,
-                               [211,170],213,[214,170],216,[217,170],219,[220,170],
-                               222,[223,170],231,[232,170],234,[235,170],237,
-                               [238,170],240,[241,170],243,[244,170],246,[247,170],
-                               249,[250,170],252,[253,170],261,[262,170],264,[265,170],
-                               267,[268,170],270,[271,170],273,[274,170],276,[277,170],
-                               279,[280,170],282,[283,170]]
+        programCruzBendita = self.cruzbendita(white)
         self.lugar = lugar
         self.nombre_archivo = 'datos_guardados.json'
         self.hardcode_luces = {
@@ -35,10 +31,65 @@ class GuardarConfiguracion:
                     {"horario_inicio": "00:00:00", "horario_fin": "05:00:00"}
                 ],
                 "modo": "automatico",
-                "canales": programAyuntamiento + programIglesia
+                "canales": programCruzBendita
             },
             
         }
+    def get_off_on_rgb(self, rgb, cont):
+        # Placeholder function: replace with actual implementation.
+        # This function needs to be defined according to the JavaScript version.
+        return [rgb, cont]
+
+    def desaguadero(self, rgb):
+        cont = 4
+        new_list = [1, 2, 3, 4]
+        for _ in range(4):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 5
+        return new_list
+
+    def ermita(self, rgb):
+        cont = 10
+        new_list = []
+        for _ in range(4):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 10
+        return new_list
+
+    def cruzbendita(self, rgb):
+        cont = 1
+        new_list = []
+        for _ in range(5):
+            new_list.append(cont)
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 8
+        
+        cont = 40
+        for _ in range(4):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 4
+        return new_list
+
+    def campanarioluces(self, rgb):
+        cont = 0
+        new_list = []
+        for _ in range(3):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 8
+        
+        cont = 24
+        for _ in range(5):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 5
+        return new_list
+
+    def espiritusanto(self, rgb):
+        cont = 49
+        new_list = []
+        for _ in range(7):
+            new_list.extend(self.get_off_on_rgb(rgb, cont))
+            cont += 8
+        return new_list
 
     def crear_archivo(self):
         try:
